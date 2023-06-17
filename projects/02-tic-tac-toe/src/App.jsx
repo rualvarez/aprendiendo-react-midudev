f726910697481f172dd0ef7fa8c89ecdd9487985
Ruben Alvarez <dev.ralvarez@gmail.com>
2023-06-17 12:45:26 +0200

    Guardar tablero y turno en localStorage

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -6,14 +6,23 @@ import { checkWinner } from './logic/board';
 import confetti from 'canvas-confetti';
 
 function App () {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [turn, setTurn] = useState(TURNS.X);
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  });
+  // const [turn, setTurn] = useState(TURNS.X);
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ? turnFromStorage : TURNS.X
+  });
   const [winner, setWinner] = useState(null);
 
   const resetGame = () => {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X)
     setWinner(null)
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   }
 
   const updateBoard = (index) => {
@@ -24,14 +33,16 @@ function App () {
     newBoard[index] = turn;
     setBoard(newBoard);
 
+    const newTurn = (turn === TURNS.X) ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
+
     const isWinner = checkWinner(newBoard)
     if (isWinner) {
       confetti()
       setWinner(isWinner)
     }
-    
-    const newTurn = (turn === TURNS.X) ? TURNS.O : TURNS.X;
-    setTurn(newTurn);
   }
 
   return (
