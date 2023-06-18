c3b201d11a85cacfa353cd938ce3346596009be2
Ruben Alvarez <dev.ralvarez@gmail.com>
2023-06-18 13:02:29 +0200

    Mostrar imagen de un gato con la primera palabra del hecho

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -2,8 +2,10 @@ import { useEffect, useState } from 'react'
 
 export function App () {
   const ENDPOINT_URL_FACT = 'https://catfact.ninja/fact'
+  const PREFIX_URL_IMAGE = 'https://cataas.com'
 
-  const [fact, setFact] = useState('')
+  const [fact, setFact] = useState()
+  const [imageUrl, setImageURL] = useState()
 
   useEffect(() => {
     fetch(ENDPOINT_URL_FACT)
@@ -11,10 +13,20 @@ export function App () {
       .then(data => setFact(data.fact))
   }, [])
 
+  useEffect(() => {
+    if (!fact) return
+    const firstWord = fact.split(' ')[0]
+    const ENDPOINT_URL_IMAGE = PREFIX_URL_IMAGE + '/cat/says/' + firstWord + '?json=true'
+    fetch(ENDPOINT_URL_IMAGE)
+      .then(res => res.json())
+      .then(data => setImageURL(data.url))
+  }, [fact])
+
   return (
     <main>
       <h1>App de gatos</h1>
       {fact && <p>{fact}</p>}
+      {imageUrl && <img src={`${PREFIX_URL_IMAGE}${imageUrl}`} />}
     </main>
   )
 }
