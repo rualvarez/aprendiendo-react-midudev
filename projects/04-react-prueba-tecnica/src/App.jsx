c0dd34ca73bdc9b184770f04b27502a22f9378fb
Ruben Alvarez <dev.ralvarez@gmail.com>
2023-06-25 14:19:55 +0200

    Seaprar en fichero el custom hook de catFact

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,18 +1,6 @@
-import { useEffect, useState } from 'react'
-import { getRandomFact } from './services/facts'
 import { PREFIX_URL_IMAGE } from './constant'
 import { useCatImage } from './hooks/useCatImage'
-
-const useCatFact = () => {
-  const [fact, setFact] = useState()
-
-  const refreshFact = () => {
-    getRandomFact().then(newFact => setFact(newFact))
-  }
-  useEffect(refreshFact, [])
-
-  return { fact, refreshFact }
-}
+import { useCatFact } from './hooks/useCarFact'
 
 export function App () {
   const { fact, refreshFact } = useCatFact()
